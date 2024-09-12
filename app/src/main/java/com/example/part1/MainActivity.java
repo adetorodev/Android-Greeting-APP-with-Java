e90383c6006af81549ee47ab5021b0e7720459f9
Ezekiel Adetoro <39957559+adetorodev@users.noreply.github.com>
2024-09-12 19:44:29 +0100

    greeting app

--- app/src/main/java/com/example/part1/MainActivity.java
+++ app/src/main/java/com/example/part1/MainActivity.java
@@ -15,31 +15,55 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    Button myBtn;
+    TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        TextView mytextView = findViewById(R.id.myText);
-        mytextView.setText("Hi From Java");
 
-        EditText myeditText = findViewById(R.id.editText);
-
-        String inputText = myeditText.getText().toString();
-
-        mytextView.setText(inputText);
-
-        Button myBtn = findViewById(R.id.myBtn);
+        editText = findViewById(R.id.edittext);
+        myBtn = findViewById(R.id.btn);
+        title = findViewById(R.id.title);
 
         myBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                    String inputName = editText.getText().toString();
 
-                    Toast.makeText(MainActivity.this,"You click the button", Toast.LENGTH_SHORT).show();
-
-            }
+                    Toast.makeText(
+                            MainActivity.this,
+                            "Welcome "+inputName,
+                            Toast.LENGTH_SHORT
+                    ).show();
+        }
         });
+
+
+
+
+//        TextView mytextView = findViewById(R.id.myText);
+//        mytextView.setText("Hi From Java");
+//
+//        EditText myeditText = findViewById(R.id.editText);
+//
+//        String inputText = myeditText.getText().toString();
+//
+//        mytextView.setText(inputText);
+//
+//        Button myBtn = findViewById(R.id.myBtn);
+
+//        myBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                    Toast.makeText(MainActivity.this,"You click the button", Toast.LENGTH_SHORT).show();
+//
+//            }
+//        });
 
     }
 }
